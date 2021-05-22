c5c2cff2633793864191e7fdb71696734a221baa
rajeshd <rajesh.de@onometra.com>
2021-05-22 21:12:40 +0530

    all context related operation is handled

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import { useState, useMemo } from "react";
+
+import { AppContext } from "../app/context";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [items, setItems] = useState([]);
+  const [details, setDetails] = useState({});
+
+  const handleOnDeleteItem = (str) => {
+    setItems(items.filter((item) => item !== str));
+  };
+  const handleOnAddItem = (str) => {
+    if (!items.includes(str)) {
+      setItems([str, ...items]);
+    }
+  };
+  const handleOnUpdateDetail = (str, detail) => {
+    console.log("id", str, "detail", detail);
+    setDetails({ ...details, [str]: detail });
+  };
+
+  const value = useMemo(
+    () => ({
+      items,
+      details,
+      handleOnDeleteItem,
+      handleOnAddItem,
+      handleOnUpdateDetail,
+    }),
+    [items, details]
+  );
+
+  return (
+    <AppContext.Provider value={value}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
